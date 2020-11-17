02063cec6cafc1e77a2c9aaae71e5e0666396783
Makke <markus215ma@gmail.com>
2020-11-17 18:13:27 +0200

    w

--- saa/src/Weather.js
+++ saa/src/Weather.js
@@ -1,26 +1,56 @@
 import React, {Component} from 'react'
-import {getForecast} from './requests'
+import {getForecasts} from './requests'
 import './Weather.css'
 
 class Weather extends Component {
-  ComponentDidMount() {
-    getForecast().then(forecasts => )
+  constructor(props) {
+    super(props)
+    this.state = {
+      forecasts: null
+    }
   }
-}
+  componentDidMount() {
+    getForecasts().then(forecasts => {
+      console.log(forecasts) //testaus
+      this.setState({ forecasts: forecasts})
+    })
+  }
 
+  renderWeatherItem(forecast) {
+    return (
+      <div className="Weather_box">
+        <div className="Weather_day">ma</div>
+        <div className="Weather_time">15:00</div>
+        <div className="Weather_temp">16°</div>
+        <img src={'http://openweathermap.org/img/wn/'+forecast.weather[0].icon+'09d.png'} alt="Logo" />
+      </div>
+    )
+  }
 
-
-
-class Weather extends Component {
   render() {
+    const forecasts = this.state.forecasts
+    if (!forecasts) return null
+    console.log(forecasts)
+
+    const focusedWeather = forecasts.list[0]
+
+
+
+
     return (
       <div className="Weather">
         <div className="Weather_focused">
-          <div className="Weather_day">ma</div>
-          <div className="Weather_time">12:00</div>
-          <div className="Weather_temp">14°</div>
+          <div className="Weather_day">{focusedWeather.dt}</div>
+          <div className="Weather_time">{focusedWeather.dt}</div>
+          <div className="Weather_temp">{focusedWeather.dt}</div>
           <img src ='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
         </div>
+
+        <div className="Weather_forecast">
+          { forecasts.list.map.(forecast => this.renderWeatherItem(forecast)}
+        </div>
+
+    
 
 
 
--- saa/src/requests.js
+++ saa/src/requests.js
@@ -1,6 +1,6 @@
 export const API_KEY = process.env.REACT_APP_KEY
 
-export const getForecast = () => new Promise(resolve => {
+export const getForecasts = () => new Promise(resolve => {
   fetch(`http://api.openweathermap.org/data/2.5/forecast?id=658225&APPID=${API_KEY}&units=metric&lang=fi`)
     .then(response => {
       if (response.status !== 200) throw new Error(response.status)
