a647a65a02457bb9ee7ef37b1fe76841bcb1b37c
markus aarrejoki <markus215ma@gmail.com>
2020-12-08 17:50:09 +0200

    Updates

---
+++ precache-manifest.54409d477b14792898f3fb9d384df976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd11f75add854cb0bbbb24877cd5f1d",
+    "url": "/aa/index.html"
+  },
+  {
+    "revision": "e15d38ac31d15b4c1713",
+    "url": "/aa/static/css/main.135534b7.chunk.css"
+  },
+  {
+    "revision": "083aafaba5b97118040a",
+    "url": "/aa/static/js/2.bff0710c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aa/static/js/2.bff0710c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15d38ac31d15b4c1713",
+    "url": "/aa/static/js/main.3ea37e89.chunk.js"
+  },
+  {
+    "revision": "32119a9795f149929ff2",
+    "url": "/aa/static/js/runtime-main.34a1013a.js"
+  }
+]);
